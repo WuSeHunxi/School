51b6baa1e4254c038308433a903d1100af3345bd
naxinyi <780957235@qq.com>
2019-11-22 19:26:02 +0800

    11.22

---
+++ 数据结构与算法/链表/链表.js
@@ -0,0 +1,23 @@
+/**
+ * 实现链表的功能
+ */
+
+function Node(value) {
+    this.value = value;
+    this.next = null;
+}
+
+var a = new Node("a");
+var b = new Node("b");
+var c = new Node("c");
+
+/**
+ * 将链表输出
+ * @param {*} root 
+ */
+function print(root) {
+    if (!root) {
+        return;
+    }
+
+}
