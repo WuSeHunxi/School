5bb39420262740aa5aa78700037d270695842b40
naxinyi <780957235@qq.com>
2019-11-23 18:06:23 +0800

    11.23

--- 数据结构与算法/排序和查找/排序.js
+++ 数据结构与算法/排序和查找/排序.js
@@ -1,10 +1,7 @@
-function swap(arr, i, j) {
-    var temp;
-    if (arr[i] > arr[j]) {
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-    }
+function swap(arr, i1, i2) {
+    var temp = arr[i1]
+    arr[i1] = arr[i2];
+    arr[i2] = temp;
 }
 /**
  * 选择排序
@@ -14,12 +11,39 @@ function selectionSort(arr) {
     for (var i = 0; i < arr.length - 1; i++) {
         var min = arr[i];
         var index = i;
-        for (var j = i; i < arr.length - 1; j++) {
-            if (min > arr[j + 1]) {
-                min = arr[j + 1];
-                index = j + 1;
+        for (var j = i + 1; i < arr.length - 1; j++) {
+            if (min > arr[j]) {
+                min = arr[j];
+                index = j;
             }
         }
         swap(arr, i, index);
     }
 }
+
+/**
+ * 冒泡排序
+ * @param {*} arr 
+ */
+function bubbleSort(arr){
+    for(var i=0;i<arr.length-1;i++){
+        for(var j=0;j<arr.length-1-i;j++){
+            if(a[j]>a[j+1]){
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+}
+
+/**
+ * 插入排序
+ * @param {*} arr 
+ */
+function insertionSort(arr){
+    
+}
+
+var arr = [5, 1, 3, 4, 7, 6];
+console.log(arr);
+selectionSort(arr);
+console.log(arr);
