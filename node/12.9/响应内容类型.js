50f5af987d2e69fb8c67d058bb76c51e25e8b338
naxinyi <780957235@qq.com>
2019-12-09 21:42:31 +0800

    12.9

--- node/12.9/响应内容类型.js
+++ node/12.9/响应内容类型.js
@@ -5,18 +5,35 @@
  */
 
 var http = require("http");
+var fs = require("fs");
 var server = http.createServer();
 server.on("request", function (request, response) {
     console.log(request.url);
     console.log("请求我的客户端的端口号是：" + request.socket.remoteAddress, request.socket.remotePort);
     var url = request.url;
-    if (url === './plain') {
-        response.setHeader("Content-type", 'text/plain;charset=UTF-8');
-        response.end("hello");
-    } else {
-        //需要告诉浏览器发送的是指定格式的文本
-        response.setHeader('Content-Type', 'text/html;charset=UTF-8')
-        response.end("<p><a href=''>点我</a></p>");
+    // if (url === './plain') {
+    //     response.setHeader("Content-type", 'text/plain;charset=UTF-8');
+    //     response.end("hello");
+    // } else {
+    //     //需要告诉浏览器发送的是指定格式的文本
+    //     response.setHeader('Content-Type', 'text/html;charset=UTF-8')
+    //     response.end("<p><a href=''>点我</a></p>");
+    // }
+    if (url === '/') {
+        //动态读取文件
+        fs.readFile("./resource/index.html", function (error, data) {
+            if (error) {
+                response.setHeader("Content-Type", "text/plain;charset=UTF-8");
+                response.end("文件读取失败，请重试");
+            } else {
+                response.setHeader("Content-Type", "text/html;charset=UTF-8");
+                response.end(data.toString());
+            }
+        })
+    } else if (url === '/01.jpeg') { //01.jpeg并非文件路径
+        //图片不需要制定编码
+        response.setHeader("Content-Type", "image/jpeg;");
+        response.end()
     }
 })
 server.listen(3000);
