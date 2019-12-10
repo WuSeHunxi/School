af109145efb2ad52e9467b0dba0ca4d6cd474e3c
naxinyi <780957235@qq.com>
2019-12-10 21:10:28 +0800

    12.10

---
+++ node/12.10/08-url.js
@@ -0,0 +1,3 @@
+var url = require("url");
+var obj = url.parse('/pinglun?name=shsh&message=jsjdd', true); //将url字符串转成url对象
+console.log(obj);
--- node/12.10/feedback/app.js
+++ node/12.10/feedback/app.js
@@ -1,30 +1,86 @@
 var http = require("http");
 var fs = require("fs");
+var template = require("art-template");
+var url = require('url');
+var comments = [{
+        name: "张三",
+        message: "哇哦",
+        dataTime: "2019-12-10"
+    },
+    {
+        name: "张三2",
+        message: "哇哦",
+        dataTime: "2019-12-10"
+    },
+    {
+        name: "张三3",
+        message: "哇哦",
+        dataTime: "2019-12-10"
+    }, {
+        name: "张三4",
+        message: "哇哦",
+        dataTime: "2019-12-10"
+    },
+    {
+        name: "张三5",
+        message: "哇哦",
+        dataTime: "2019-12-10"
+    }
+]
+/**
+ * 
+ * 本服务器的作用-->规定那些资源能够被用户访问 
+ * 
+ */
 http.createServer(function (request, response) {
-    var url = request.url;
+    var parseObj = url.parse(request.url, true);
+    //单独获取不包含查询字符串的部分（该路径不包含问号之后的那些内容）
+    var pathname = parseObj.pathname;
+
+    /**
+     * 下面的代码代表的是我让你读了你才能够访问的到（只有满足if else的条件才能够读取）
+     * 
+     */
     //只公开了public文件夹和index
-    if (url === '/') {
+    if (pathname === '/') {
         fs.readFile('./views/index.html', function (error, data) {
             if (error) {
                 return response.end('404 Not Found');
             }
-            response.end(data);
+            var htmlStr = template.render(data.toString(), {
+                comments: comments
+            })
+            response.end(htmlStr);
         })
-    }
-    //处理静态资源 (开放静态资源) 在index.html中请求资源文件
-    else if (url.indexOf('/public/') === 0) {
-        //把请求路径当成文件路径
-        //哪些资源可以被用户访问，哪些不可以需要用代码控制
-        fs.readFile('.' + url, function (error, data) {
+    } else if (pathname === '/post') {
+        fs.readFile('./views/post.html', function (error, data) {
             if (error) {
                 return response.end("404 Not Found");
             }
             response.end(data);
         })
-        console.log(url);
+    }
+    //处理静态资源 (开放静态资源) 在index.html中请求资源文件
+    else if (pathname.indexOf('/public/') === 0) {
+        //把请求路径当成文件路径
+        //哪些资源可以被用户访问，哪些不可以需要用代码控制
+        fs.readFile('.' + pathname, function (error, data) {
+            if (error) {
+                return response.end("404 Not Found");
+            }
+            response.end(data);
+        })
+        console.log(pathname);
+    } else if (pathname === '/pinglun') {
+        //无论/pinglun后是什么都无需担心了
+        // console.log("收到请求", parseObj.query);        
+        response.end(JSON.stringify(parseObj.query));
     } else {
         fs.readFile('./views/404.html', function (error, data) {
-
+            if (error) {
+                return response.end("404 Not Found");
+            }
+            response.end(data);
         })
     }
 }).listen(3000, function () {
