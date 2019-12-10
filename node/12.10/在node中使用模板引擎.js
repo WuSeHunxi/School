24e468013e0722a7a51d1b6a13aff498f90c1a5b
naxinyi <780957235@qq.com>
2019-12-10 14:59:04 +0800

    12.10

--- node/12.10/在node中使用模板引擎.js
+++ node/12.10/在node中使用模板引擎.js
@@ -10,11 +10,24 @@
 
 //加载核心模块  在使用npm下载时install的是谁require中的就是谁
 var template = require("art-template");
+var fs = require("fs");
+fs.readFile("./tpl.html", function (error, data) {
+    if (error) {
+        return console.log("读取文件失败了");
+    }
+    var ret = template.render(data.toString(), {
+        name: "kaka",
+        age: 18,
+        provice: '北京',
+        hobbies: ['写代码', '学习', '看视频']
+    })
+    console.log(ret);
+})
 
 //核心方法
 template.render('模板字符串', '替换对象');
 
-var ret = template.render('hello{{name}}', {
-    name: "pappa"
-});
-console.log(ret);
+// var ret = template.render('hello{{name}}', {
+//     name: "pappa"
+// });
+// console.log(ret);
