98abb6067288b17056a415fc69b39ae809edb21d
naxinyi <780957235@qq.com>
2019-12-11 17:06:18 +0800

    12.11

--- node/12.11/express-demo/app.js
+++ node/12.11/express-demo/app.js
@@ -12,6 +12,29 @@ app.get('/', function (req, res) {
     res.send('hello express!');
 })
 
+//此时请求其他的路径的时候不需要再if-else了，可以直接使用xx.get进行路径修改
+app.get('/about', function (req, res) {
+    // res.send('world express!');
+    //直接将下面的字符串进行编码了，不需要再手动写了
+    res.send(
+        `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>hello express</h1>
+</body>
+
+</html>`
+    );
+})
+
 //-->server.listen
 app.listen(3000, function () {
     console.log('app is running ar prot 3000');
