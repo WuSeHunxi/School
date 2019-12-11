ce70b2fd0e448a0fc764b1832fd2b6eaf55dab82
naxinyi <780957235@qq.com>
2019-12-11 09:16:36 +0800

    12.11

---
+++ node/12.11/模块系统/模块.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Node中的模块系统：
+ *      EcmaScript语言：没有BOM和DOM
+ *      核心模块：
+ *          文件操作fs
+ *          http服务
+ *          url路径模块
+ *          path路径处理模块
+ *          os操作系统模块
+ *      第三方模块：art-template（需要安装）
+ *      自己写的
+ */
+
