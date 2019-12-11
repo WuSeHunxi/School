abcf7e70ca65c657ae708a308eefc3dccff4c658
naxinyi <780957235@qq.com>
2019-12-11 11:02:48 +0800

    12.11

--- node/12.11/模块系统/foo.js
+++ node/12.11/模块系统/foo.js
@@ -1,3 +1,24 @@
 var foo = 'bar'
 
-exports.foo = foo;
+// exports.foo = foo;
+
+/**
+ * 导出一个对象：exports.x=xxx;
+ * 导出某个成员：module.exports=xxx;--->后者会覆盖前者
+ */
+
+//如果一个模块需要直接导出某个成员，而非挂在的话需要用module.exports
+module.exports = foo;
+
+module.exports = function (x, y) {
+    return x + y;
+}
+
+
+module.exports = { //此时导出的是一个对象
+    add: function (x, y) {
+        return x * y;
+    },
+    str: 'hello'
+}
+
--- node/12.11/模块系统/main.js
+++ node/12.11/模块系统/main.js
@@ -1,1 +1,7 @@
-require("./foo");
+//加载
+var ret = require("./foo");
+// console.log(ret.foo);//得到的是一个对象
+// console.log(ret(1, 2)); //-->bar
+
+
+console.log(ret); //此时得到的是一个对象
