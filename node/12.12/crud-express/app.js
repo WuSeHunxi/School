01b9b2bfb8fb76a70ef27c441d389f187c59fb7b
naxinyi <780957235@qq.com>
2019-12-12 15:22:17 +0800

    12.12

--- node/12.12/crud-express/app.js
+++ node/12.12/crud-express/app.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+app.use('/node_modules/', express.static('./node_modules'));
+app.engine('html', require("express-art-template"));
 app.get('/', function (req, res) {
+    res.render('index.html');
 
+})
+app.listen(3000, function () {
+    console.log('running...');
 })
