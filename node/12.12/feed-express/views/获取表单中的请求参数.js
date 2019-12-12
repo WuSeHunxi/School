748aa0a3459af7f543ff71c9710a549182150600
naxinyi <780957235@qq.com>
2019-12-12 14:48:34 +0800

    12.12

--- node/12.12/feed-express/views/获取表单中的请求参数.js
+++ node/12.12/feed-express/views/获取表单中的请求参数.js
@@ -9,5 +9,11 @@
                     extended: false
                 }))
                 app.use(bodyParser.json());
+            使用：
+                app.use(function(req,res){
+                    res.setHeader('Content-Type),'tetx/plain;
+                    res.write('you posted:\n');
+                    res.end(JSON.stringify(req.body,null,2));
+                })
  * 
  */
