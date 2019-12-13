2bb1f41714e2206afbe9de0e52b29ec1373a9467
naxinyi <780957235@qq.com>
2019-12-13 20:25:48 +0800

    12.13

--- node/12.13/复习.js
+++ node/12.13/复习.js
@@ -1,5 +1,18 @@
 /**
  * 一般情况下，把函数作为参数的目的就是为了获取函数内部的异步操作结果
+ * 异步操作：setTimeout,readFile,writeFile,ajax（因为他们都需要接收一个回调函数）
  * 代码在执行的过程中不会等待定时器
  * js的事件循环，单线程
+ * 
  */
+
+//callback是回调函数
+function add(x, y, callback) {
+    setTimeout(() => {
+        var ret = x + y;
+        callback(ret); //回调函数
+    }, 1000);
+}
+add(10, 20, function (ret) {
+    console.log(ret);
+})
