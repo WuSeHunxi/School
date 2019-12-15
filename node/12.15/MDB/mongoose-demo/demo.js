58373643d22160ee80fc71283deef3e91a336f14
naxinyi <780957235@qq.com>
2019-12-15 15:42:44 +0800

    12.15

--- node/12.15/MDB/mongoose-demo/demo.js
+++ node/12.15/MDB/mongoose-demo/demo.js
@@ -2,17 +2,24 @@
  * 连接操作数据库
  */
 
+//引包
 var mongoose = require("mongoose");
+//连接MongoDB数据库
 mongoose.connect('mongoose://loclhost/test', {
     useMongoClient: true
 });
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+
+//在MongoDB中数据库表是集合，相当于一个数组
+//创建一个模型-->设计一个数据库，MongoDB是动态的：Cat是表名，第二个参数表示name是字符串类型
 var Cat = mongoose.model('Cat', {
     name: String
 });
+//实例化一个Cat
 var kitty = new Cat({
     name: 'Zildjian'
 });
+//持久化保存kitty实例
 kitty.save(function (err) {
     if (err) {
         console.log(err);
